ee007bd744bad9459acaaeeaf77921235c30eb15
github_Loic069 <loicmuller2001@outlook.fr>
2020-06-16 20:53:32 +0200

    Background Header

--- chat-app/src/components/AuthPage.js
+++ chat-app/src/components/AuthPage.js
@@ -3,25 +3,32 @@ import React, { Component } from 'react'
 export default class AuthPage extends Component {
     render() {
         return (
-            <div className="container">
+            <div>
 
-                <h1 className="font text-center m-5">Bienvenue sur Converso</h1>
-                
-                <div className="row justify-content-around">
-
-                    <div className="col-xs-2 authButton">
-                        <button className="p-3 m-4 btn btn-outline-dark">
-                            SE CONNECTER
-                        </button>
+                <header>
+                    <div className="banner-header">
+                        <h1 className="font text-center"><a>CONVERSO</a></h1>
                     </div>
-                    <div className="col-xs-2 authButton">
-                        <button className="p-3 m-4 btn btn-outline-dark">
-                            S'INSCRIRE
-                        </button>
-                    </div>
+                </header>
 
-                </div> 
+                <div className="container">
+                    
+                    <div className="row justify-content-around authButton">
 
+                        <div className="col-xs-2">
+                            <button className="p-3 m-4 btn btn-outline-dark">
+                                SE CONNECTER
+                            </button>
+                        </div>
+                        <div className="col-xs-2">
+                            <button className="p-3 m-4 btn btn-outline-dark">
+                                S'INSCRIRE
+                            </button>
+                        </div>
+
+                    </div> 
+
+                </div>
             </div>
         )
     }
