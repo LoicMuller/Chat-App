c8226bdd37a583294a816daebf11cec0934d0787
github_Loic069 <loicmuller2001@outlook.fr>
2020-06-17 12:39:36 +0200

    initializing login/signup file

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Route, Switch, Redirect, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import AuthPage from './components/AuthPage';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
 
 function App() {
   return (
     <div className="App">
-      <AuthPage />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={AuthPage} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- chat-app/src/components/AuthPage.js
+++ chat-app/src/components/AuthPage.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { NavLink, Link, Redirect } from 'react-router-dom';
 
 export default class AuthPage extends Component {
     render() {
