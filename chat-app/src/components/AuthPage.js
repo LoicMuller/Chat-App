b084da6aa0610fcf2f29679211cb79423a4115d1
github_Loic069 <loicmuller2001@outlook.fr>
2020-06-17 12:14:22 +0200

    Auth Page completed

--- chat-app/src/components/AuthPage.js
+++ chat-app/src/components/AuthPage.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react'
 export default class AuthPage extends Component {
     render() {
         return (
-            <div>
+            <div className="authPage">
 
                 <header>
                     <div className="banner-header">
-                        <h1 className="font text-center"><a>CONVERSO</a></h1>
+                        <h1 className="font text-center"><a href="/">CONVERSO</a></h1>
                     </div>
                 </header>
 
@@ -16,12 +16,12 @@ export default class AuthPage extends Component {
                     <div className="row justify-content-around authButton">
 
                         <div className="col-xs-2">
-                            <button className="p-3 m-4 btn btn-outline-dark">
+                            <button className="p-3 m-4 btn btn-outline-dark btn-lg">
                                 SE CONNECTER
                             </button>
                         </div>
                         <div className="col-xs-2">
-                            <button className="p-3 m-4 btn btn-outline-dark">
+                            <button className="p-3 m-4 btn btn-outline-dark btn-lg">
                                 S'INSCRIRE
                             </button>
                         </div>
