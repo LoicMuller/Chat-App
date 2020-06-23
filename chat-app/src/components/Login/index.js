ba578a40ee0984c71f4e96da88ab43bdc585be28
github_Loic069 <loicmuller2001@outlook.fr>
2020-06-23 17:41:57 +0200

    Login Method completed

--- chat-app/src/components/Login/index.js
+++ chat-app/src/components/Login/index.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { FirebaseContext } from '../Firebase';
 
-const Login = () => {
+const Login = props => {
+    const firebase = useContext(FirebaseContext);
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [btn, setBtn] = useState(false);
+    const [error, setError] = useState('');
+
+    useEffect(() => {
+        if (password.length > 5 && email !== '') {
+            setBtn(true);
+        } else if (btn) {
+            setBtn(false);
+        }
+    }, [password, email, btn]);
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        firebase.signInUser(email, password)
+        .then(user => {
+            setEmail('');
+            setPassword('');
+            props.history.push('/home');
+        })
+        .catch(error => {
+            setError(error);
+            setEmail('');
+            setPassword('');
+        })
+    };
+
     return (
         <div className="text-center">
 
@@ -11,19 +43,23 @@ const Login = () => {
 
             {/* Login Form */}
             <div id="loginBox">
+
+                { error !== '' && <span>{error.message}</span> }
+
                 <h6 className="display-4">Login</h6>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="inputBox">
-                        <input  type="email" autoComplete="off" required />
+                        <input onChange={e => setEmail(e.target.value)} value={email} type="email" autoComplete="off" required />
                         <label htmlFor="email">Email</label>
                     </div>
                     
                     <div className="inputBox">
-                        <input  type="password" autoComplete="off" required />
+                        <input onChange={e => setPassword(e.target.value)} value={password} type="password" autoComplete="off" required />
                         <label htmlFor="password">Password</label>
                     </div>
 
-                    <button>Log in</button>
+                    { btn ? <button>Log In</button> : <button disabled>Log In</button> }
+
                 </form>
                 <div className="linkContainer">
                     <Link className="simpleLink" to="/register">Do not have an account yet?</Link>
