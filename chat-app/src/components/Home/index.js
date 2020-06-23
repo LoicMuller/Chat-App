c1a011bf047104861c4f513c6917a0dd099d1b3b
github_Loic069 <loicmuller2001@outlook.fr>
2020-06-23 17:43:07 +0200

    Start Creating the home page

---
+++ chat-app/src/components/Home/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from '../Navbar';
+
+const Home = () => {
+    return (
+        <Navbar />
+        // - "Nice to see you, {username}"
+        // - Background bleu-marine/gris, texte plus clair
+        // - Possibilité de cliquer sur l'îcone d'un de ses amis pour commencer
+        // à discuter...
+        // - If (amis == 0) return "Click here to add some friends" ->
+        // redirect to="/addFriends" (explications sur comment inviter) ->
+        // envoie un lien à l'username écrit si il existe, sinon msg d'erreur
+    )
+}
+
+export default Home
