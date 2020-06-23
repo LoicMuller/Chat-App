743d4f55c7c4e5b4c6edb39f3d6451fbc746c6af
github_Loic069 <loicmuller2001@outlook.fr>
2020-06-23 17:42:46 +0200

    Creating home route

--- chat-app/src/components/App/index.js
+++ chat-app/src/components/App/index.js
@@ -10,6 +10,7 @@ import Footer from '../Footer';
 import Login from '../Login';
 import Register from '../Register';
 import ForgetPassword from '../ForgetPassword';
+import Home from '../Home';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route path="/login" component={Login} />
           <Route path="/register" component={Register} />
           <Route path="/forgetpassword" component={ForgetPassword} />
+          <Route path="/home" component={Home} />
         </Switch>
         <Route path="/" component={Footer} />
       </Router>
