cae1c22d746be476c1b78817d19c854d353422aa
github_Loic069 <loicmuller2001@outlook.fr>
2020-06-23 15:52:00 +0200

    SignUp method 95% complete

--- chat-app/src/components/Register/index.js
+++ chat-app/src/components/Register/index.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { FirebaseContext } from '../Firebase';
 import { Link } from 'react-router-dom';
 
 const Register = () => {
+
+    const firebase = useContext(FirebaseContext);
+
+    const data = {
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+    };
+
+    const [loginData, setLoginData] = useState(data);
+    const [error, setError] = useState('');
+
+    const handleChange = e => {
+        setLoginData({ ...loginData, [e.target.id]: e.target.value });
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const { email, password } = loginData;
+        firebase.signUpUser(email, password)
+        .then(user => {
+            setLoginData({ ...data });
+        })
+        .catch(error => {
+            setError(error);
+            setLoginData({ ...data });
+        })
+    };
+
+    const { username, email, password, confirmPassword } = loginData;
+
+    const btn = username === '' || email === '' || password === '' || password !== confirmPassword
+    ? <button disabled>Sign Up</button> : <button>Sign Up</button>
+
+    // Errors
+    const errorMsg = error !== '' && <span>{error.message}</span>;
+    
     return (
         <div className="text-center">
 
@@ -11,27 +50,30 @@ const Register = () => {
 
             {/* Register Form */}
             <div id="signUpBox">
+
+                {errorMsg}
+
                 <h6 className="display-4">Sign Up</h6>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="inputBox">
-                        <input  type="text" id="username" autoComplete="off" required />
+                        <input onChange={handleChange} value={username} type="text" id="username" autoComplete="off" required />
                         <label htmlFor="username">Username</label>
                     </div>
                     
                     <div className="inputBox">
-                        <input  type="email" id="email" autoComplete="off" required />
+                        <input onChange={handleChange} value={email} type="email" id="email" autoComplete="off" required />
                         <label htmlFor="email">Email</label>
                     </div>
                     <div className="inputBox">
-                        <input  type="password" id="password" autoComplete="off" required />
+                        <input onChange={handleChange} value={password} type="password" id="password" autoComplete="off" required />
                         <label htmlFor="password">Password</label>
                     </div>
                     <div className="inputBox">
-                        <input  type="password" id="confirmPassword" autoComplete="off" required />
+                        <input onChange={handleChange} value={confirmPassword} type="password" id="confirmPassword" autoComplete="off" required />
                         <label htmlFor="confirmPassword">Confirm password</label>
                     </div>
 
-                    <button>Sign Up</button>
+                    {btn}
                 </form>
                 <div className="linkContainer">
                     <Link className="simpleLink" to="/login">Already have an account?</Link>
