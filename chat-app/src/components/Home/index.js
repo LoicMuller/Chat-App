ea40a1de777a289e2463fed79f97428c18294873
github_Loic069 <loicmuller2001@outlook.fr>
2020-06-24 15:01:28 +0200

    Updating user session

--- chat-app/src/components/Home/index.js
+++ chat-app/src/components/Home/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import Navbar from '../Navbar';
+import { useContext, useState, useEffect } from 'react';
+import { FirebaseContext } from '../Firebase';
 
-const Home = () => {
-    return (
+const Home = props => {
+
+    const firebase = useContext(FirebaseContext);
+
+    const [userSession, setUserSession] = useState(null);
+
+    useEffect(() => {
+        let listener = firebase.auth.onAuthStateChanged(user => {
+            user ? setUserSession(user) : props.history.push('/');
+        });
+
+        return () => {
+            listener();
+        };
+
+    }, [userSession, firebase, props.history])
+
+    return userSession === null ? (
+        <>
+            <div id="backgroundColor"></div>
+            <div className="loader"></div>
+            <p className="loaderText">Loading ...</p>
+        </> 
+    ) : (
         <Navbar />
         // - "Nice to see you, {username}"
         // - Background bleu-marine/gris, texte plus clair
