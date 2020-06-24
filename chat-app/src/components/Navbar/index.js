aa7f26d9b194e3a88a5ccdcb0203c57561a07e61
github_Loic069 <loicmuller2001@outlook.fr>
2020-06-24 14:35:10 +0200

    Sign out user method completed

--- chat-app/src/components/Navbar/index.js
+++ chat-app/src/components/Navbar/index.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FirebaseContext } from '../Firebase';
 
 const Navbar = () => {
+
+    const firebase = useContext(FirebaseContext);
+
+    const history = useHistory();
+
+    const handleClick = e => {
+        e.preventDefault();
+        firebase.signOutUser()
+        .then(() => {
+            history.push('/');
+        })
+    }
+
     return (
-        <div>
-            {/* - Profile, - Friends, - Groups , - Logout */}
-        </div>
+        <nav>
+            <div id="backgroundColor"></div>
+            <ul className="navList">
+                <li><Link to="/profile">Profile</Link></li>
+                <li><Link to="/friends">Friends</Link></li>
+                <li><Link to="/groups">Groups</Link></li>
+                <li><Link to="/" onClick={handleClick}>Logout</Link></li>
+            </ul>
+        </nav>
     )
 }
 
