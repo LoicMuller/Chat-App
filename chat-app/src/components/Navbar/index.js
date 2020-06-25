6ff52f57346fbb58e5e8a0e36a8d65ab1245982e
github_Loic069 <loicmuller2001@outlook.fr>
2020-06-25 15:09:52 +0200

    Responsive navbar animation

--- chat-app/src/components/Navbar/index.js
+++ chat-app/src/components/Navbar/index.js
@@ -16,6 +16,11 @@ const Navbar = () => {
         })
     }
 
+    const openSideBar = event => {
+        event.preventDefault();
+        document.querySelector('.sideBar').classList.toggle('active');
+    }
+
     return (
         <>
             <div id="backgroundColor"></div>
@@ -25,8 +30,16 @@ const Navbar = () => {
                     <li><Link to="/friends">Friends</Link></li>
                     <li><Link to="/groups">Groups</Link></li>
                     <li><Link to="/" onClick={handleClick}>Logout</Link></li>
-
-                    <i className="fas fa-bars" id="navIcon"></i>
+                    
+                    <i className="fas fa-bars" id="navIcon" onClick={openSideBar}></i>
+                    <div className="sideBar">
+                        <ul>
+                            <li><Link to="/profile">Profile</Link></li>
+                            <li><Link to="/friends">Friends</Link></li>
+                            <li><Link to="/groups">Groups</Link></li>
+                            <li><Link to="/" onClick={handleClick}>Logout</Link></li>
+                        </ul>
+                    </div>
                 </ul>
             </nav>
         </>
