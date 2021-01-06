cc293dbcfaf722cb37cf300465ee83f506d7875b
Loic Muller <loicdev069@gmail.com>
2021-01-06 18:15:59 +0100

    fix bugs

--- chat-app/src/components/Chat/index.js
+++ chat-app/src/components/Chat/index.js
@@ -1,114 +1,116 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { FirebaseContext } from '../Firebase';
-import queryString from 'query-string';
-import io from 'socket.io-client';
-import InfoBar from '../InfoBar';
-import Input from '../Input';
-import Messages from '../Messages';
-import TextContainer from '../TextContainer';
-import Navbar from '../Navbar';
+import React, { useState, useEffect, useContext } from "react";
+import { FirebaseContext } from "../Firebase";
+import queryString from "query-string";
+import io from "socket.io-client";
+import InfoBar from "../InfoBar";
+import Input from "../Input";
+import Messages from "../Messages";
+import TextContainer from "../TextContainer";
+import Navbar from "../Navbar";
 
-import './chat.css';
+import "./chat.css";
 
 let socket;
 
 const Chat = ({ location, history }) => {
-    const ENDPOINT = 'https://converso-app.herokuapp.com/';
+  const ENDPOINT = "https://converso-app.herokuapp.com/";
 
-    const firebase = useContext(FirebaseContext);
+  const firebase = useContext(FirebaseContext);
 
-    const [userSession, setUserSession] = useState(null);
+  const [userSession, setUserSession] = useState(null);
 
-    const [userData, setUserData] = useState({});
+  const [userData, setUserData] = useState({});
 
-    const username = userData.username;
+  const username = userData.username;
 
-    const [name, setName] = useState('');
-    const [room, setRoom] = useState('');
-    const [users, setUsers] = useState('');
-    const [message, setMessage] = useState('');
-    const [messages, setMessages] = useState([]);
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const [users, setUsers] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
 
-    useEffect(() => {
-        const { name, room } = queryString.parse(location.search);
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
 
-        socket = io(ENDPOINT);
+    socket = io(ENDPOINT);
 
-        setRoom(room);
-        setName(name);
+    setRoom(room);
+    setName(name);
 
-        socket.emit('join', { name, room }, (error) => {
-            if(error) {
-                alert(error);
-            }
-        });
-    }, [ENDPOINT, location.search]);
+    socket.emit("join", { name, room }, (error) => {
+      if (error) {
+        alert(error);
+      }
+    });
+  }, [ENDPOINT, location.search]);
 
-    useEffect(() => {
-        socket.on('message', (message) => {
-            setMessages(messages => [ ...messages, message ]);
-        });
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages((messages) => [...messages, message]);
+    });
 
-        socket.on('roomData', ({ users }) => {
-            setUsers(users);
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
+    });
+  }, []);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    if (message) {
+      socket.emit("sendMessage", message, () => setMessage(""));
+    }
+  };
+
+  useEffect(() => {
+    let listener = firebase.auth.onAuthStateChanged((user) => {
+      user ? setUserSession(user) : history.push("/");
+    });
+
+    if (!!userSession) {
+      firebase
+        .user(userSession.uid)
+        .get()
+        .then((doc) => {
+          if (doc && doc.exists) {
+            const myData = doc.data();
+            setUserData(myData);
+          }
         })
-    }, []);
-
-    const sendMessage = (event) => {
-        event.preventDefault();
-
-        if(message) {
-            socket.emit('sendMessage', message, () => setMessage(''));
-        }
+        .catch((error) => {
+          console.log(error);
+        });
     }
 
-    console.log(message, messages);
+    return () => {
+      listener();
+    };
+  }, [userSession, firebase, history]);
 
-    useEffect(() => {
-        let listener = firebase.auth.onAuthStateChanged(user => {
-            user ? setUserSession(user) : history.push('/');
-        });
-
-        if (!!userSession) {
-            firebase.user(userSession.uid)
-            .get()
-            .then( doc => {
-                if (doc && doc.exists) {
-                    const myData = doc.data();
-                    setUserData(myData);
-                }
-            })
-            .catch(error => {
-                console.log(error);
-            });
-        }
-        
-        return () => {
-            listener();
-        };
-
-    }, [userSession, firebase, history])
-
-    return userSession === null ? (
-        <>
-            <div id="backgroundColor"></div>
-            <div className="loader"></div>
-            <p className="loaderText">Loading ...</p>
-        </> 
-    ) : (
-        <>
-            <Navbar />
-            <div className="outerContainer">
-                <div id="background"></div>
-                <div className="innerContainer">
-                    <InfoBar room={room}/>
-                    <Messages messages={messages} name={name}/>
-                    <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
-                </div>
-                <TextContainer users={users}/>
-            </div>
-        </>
-    )
-}
+  return userSession === null ? (
+    <>
+      <div id="backgroundColor"></div>
+      <div className="loader"></div>
+      <p className="loaderText">Loading ...</p>
+    </>
+  ) : (
+    <>
+      <Navbar />
+      <div className="outerContainer">
+        <div id="background"></div>
+        <div className="innerContainer">
+          <InfoBar room={room} />
+          <Messages messages={messages} name={name} />
+          <Input
+            message={message}
+            setMessage={setMessage}
+            sendMessage={sendMessage}
+          />
+        </div>
+        <TextContainer users={users} />
+      </div>
+    </>
+  );
+};
 
 export default Chat;
